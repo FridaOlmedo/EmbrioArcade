e92aa7a347a7beca4f44a58ca429e3691d1cac87
FridaOlmedo <a373797@uach.mx>
2025-05-09 11:11:34 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/jugar')
+def jugar():
+    return render_template('jugar.html')  # Esta es la nueva página para jugar
+
+@app.route('/espermatozoide')
+def espermatozoide():
+    return render_template('templates/espermatozoide.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
